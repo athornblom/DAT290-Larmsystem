d7478371c3f36a6e8240cc79a8e38fbf4609502f
GFraus <45488263+GFraus@users.noreply.github.com>
2019-09-19 14:06:16 +0200

    Uppdaterade main och app_init

--- kod/Door/startup.c
+++ kod/Door/startup.c
@@ -6,72 +6,91 @@
 #include "stm32f4xx.h"
 #include "stm32f4xx_rcc.h"
 
+void startup(void) __attribute__((naked)) __attribute__((section(".start_section")));
 
-void startup(void) __attribute__((naked)) __attribute__((section (".start_section")) );
-
-void startup ( void )
+void startup(void)
 {
-__asm volatile(
-	" LDR R0,=0x2001C000\n"		/* set stack */
-	" MOV SP,R0\n"
-	" BL main\n"				/* call main */
-	"_exit: B .\n"				/* never return */
-	) ;
+	__asm volatile(
+		" LDR R0,=0x2001C000\n" /* set stack */
+		" MOV SP,R0\n"
+		" BL main\n"   /* call main */
+		"_exit: B .\n" /* never return */
+	);
 }
 
-void app_init(){
-/*  Function used to set the GPIO configuration to the default reset state ****/
+void Port_init()
+{
+	/*  Function used to set the GPIO configuration to the default reset state ****/
 	GPIO_InitTypeDef init;
-	
+
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 	GPIO_StructInit(&init);
 	init.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_3 | GPIO_Pin_5 | GPIO_Pin_7;
 	init.GPIO_Mode = GPIO_Mode_OUT;
 	init.GPIO_OType = GPIO_OType_PP;
-	GPIO_Init(GPIOA,&init);
-	
+	GPIO_Init(GPIOA, &init);
 
-
-//konfigurerar inport
+	//konfigurerar inport
 	GPIO_StructInit(&init);
 	init.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_2 | GPIO_Pin_4 | GPIO_Pin_6;
 	init.GPIO_Mode = GPIO_Mode_IN;
 	init.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_Init(GPIOA,&init);
-	
-	
+	GPIO_Init(GPIOA, &init);
 }
+
+void app_init()
+{
+}
+
 void main(void)
 {
-	app_init();
+	//app_init();
+	Port_init();
 
+	uint16_t GPIO_Pins[] = {
+		GPIO_Pin_0, GPIO_Pin_1, GPIO_Pin_2, GPIO_Pin_3,
+		GPIO_Pin_4, GPIO_Pin_5, GPIO_Pin_6, GPIO_Pin_7,
+		GPIO_Pin_8, GPIO_Pin_9, GPIO_Pin_10, GPIO_Pin_11,
+		GPIO_Pin_12, GPIO_Pin_13, GPIO_Pin_14, GPIO_Pin_15};
+
+	while (1)
+	{
+		for (int i = 0; i < sizeof(GPIO_Pins); i = i + 2)
+		{
+			if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pins[i]))
+			{
+				GPIO_ResetBits(GPIOA, GPIO_Pins[i + 1]);
+			}
+			else
+			{
+				GPIO_SetBits(GPIOA, GPIO_Pins[i + 1]);
+			}
+		}
+	}
+	/*
 	while (1) {
-		if (GPIO_ReadInputData(GPIOA) & (1<<2)) {
+		if (GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0)) {
 			GPIO_ResetBits(GPIOA,GPIO_Pin_1);
 		} else {
 			GPIO_SetBits(GPIOA,GPIO_Pin_1);
+			// 
 		}
-		if (GPIO_ReadInputData(GPIOA) & (1<<4)) {
+		if (GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_2)) {
 			GPIO_ResetBits(GPIOA,GPIO_Pin_3);
 		} else {
 			GPIO_SetBits(GPIOA,GPIO_Pin_3);
 		}
-		if (GPIO_ReadInputData(GPIOA) & (1<<6)) {
+		if (GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_4)) {
 			GPIO_ResetBits(GPIOA,GPIO_Pin_5);
 		} else {
 			GPIO_SetBits(GPIOA,GPIO_Pin_5);
-		
 		}
+		
 	}
-	
+	*/
+
 	//struct door* newItem = malloc(sizeof(struct door));
-	
-	
 }
-
-	
-	
-
 
 /*inline struct door* door(char id, char controlbits, char time_larm,char time_central_larm, short password)
 {
@@ -79,14 +98,13 @@ void main(void)
 }
 */
 
-struct door {
+struct door
+{
 	char id;
-	char controlbits; // 8 kontrollbitar tex den minst signifikanta biten är ifall dörren är upplåst eller ej
-	char time_larm; // tid i 10 sekunders intervall innan dörr larmar lokalt
+	char controlbits;		// 8 kontrollbitar tex den minst signifikanta biten är ifall dörren är upplåst eller ej
+	char time_larm;			// tid i 10 sekunders intervall innan dörr larmar lokalt
 	char time_central_larm; // tid i 10 sekunders intervall innan dörr larmar centralenheten
-	short password; //4 sifferig kod för att låsa upp dörrarna
+	short password;			//4 sifferig kod för att låsa upp dörrarna
 };
 
-
-
-// Array av arrayer för Centralenheten, 2d, en siffra för kortet, en siffra för dörrarna, 
+// Array av arrayer för Centralenheten, 2d, en siffra för kortet, en siffra för dörrarna,
