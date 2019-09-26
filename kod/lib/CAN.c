478fb948e0e0a19fe6b414d5dfc67889edc216cd
Josef Karlsson <josefka@student.chalmers.se>
2019-09-26 11:32:09 +0200

    Ställer in datalängd och id i encode-funktionen

--- kod/lib/CAN.c
+++ kod/lib/CAN.c
@@ -122,6 +122,10 @@ uint8_t can_init() {
 uint8_t encode_door_time_config(CanTxMsg *msg, uint32_t time0, uint32_t time1){
     uint8_t *data_pointer =  &(msg->Data);
     
+    msg->StdId = 0;
+    msg->ExtId = 0;
+    msg->DLC = 8;
+    
     //De två tidsvärdena skrivs in i bytearrayen för data
     *data_pointer = time0;
     *(data_pointer + 4) = time1;
@@ -129,7 +133,7 @@ uint8_t encode_door_time_config(CanTxMsg *msg, uint32_t time0, uint32_t time1){
     return 1;
 }
 
-uint8_t handle_door_time_msg(CanTxMsg *msg) {
+uint8_t handle_door_time_msg(CanRxMsg *msg) {
     uint32_t time0, time1;
     uint8_t *data_pointer =  &(msg->Data);
     
@@ -138,3 +142,4 @@ uint8_t handle_door_time_msg(CanTxMsg *msg) {
     
     //TODO: Gör grejer med tiderna
 }
+
