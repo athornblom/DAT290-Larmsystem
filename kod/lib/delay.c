ae1634a90e073815f232acc93350abcc56e95751
Filip Borg <filipborg97@gmail.com>
2019-10-01 09:47:46 +0200

    Lägger till en blockerande delay

---
+++ kod/lib/delay.c
@@ -0,0 +1,9 @@
+#include "delay.h"
+
+void blockingDelayus(uint16_t delay){
+    SysTick->CTRL = 0;
+    SysTick->LOAD = 168 * delay;
+    SysTick->CTRL = SysTick_CTRL_ENABLE_Msk | SysTick_CTRL_CLKSOURCE_Msk;
+    while (!(SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk));
+    SysTick->CTRL = 0;
+}
