4866d619ea9d2ea47d1902322461d9a1811ca514
GFraus <45488263+GFraus@users.noreply.github.com>
2019-10-08 21:02:32 +0200

    Testar detekterings/doorInit
    Alla dörrar verkar initieras korrekt, men av någon anledning så verkar alla dörrar förutom dörr 1 larma lokalt efter 10 sekunder när systemet körs igång. TROTS att time_larm sätts till något annat än 10. Felsökning fortsätter.

--- kod/Door/startup.c
+++ kod/Door/startup.c
@@ -29,32 +29,20 @@ void startup(void)
 uint16_t GPIO_Pins[] = {
 	GPIO_Pin_0, GPIO_Pin_1, GPIO_Pin_2, GPIO_Pin_3, GPIO_Pin_4, GPIO_Pin_5,
 	GPIO_Pin_6, GPIO_Pin_7, GPIO_Pin_8, GPIO_Pin_9, GPIO_Pin_10, GPIO_Pin_11,
-	GPIO_Pin_12, GPIO_Pin_13, GPIO_Pin_14, GPIO_Pin_15
-	};
-
+	GPIO_Pin_12, GPIO_Pin_13, GPIO_Pin_14, GPIO_Pin_15};
 
 volatile uint32_t msTicks = 0; /* Variable to store millisecond ticks */
-int activeDoors = 0;
 
-void number_of_activeDoors (void){
+void detect_Closed_Doors(int *pointer)
+{
 
-	for (int i = 0; i < sizeof(GPIO_Pins); i = i+2)
+	for (int i = 0; i < sizeof(GPIO_Pins); i = i + 2)
 	{
-		if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pins[i]));
+		if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pins[i]))
+			;
 		{
-			activeDoors++;
+			(*pointer)++;
 		}
-		
-	}
-}
-
-
-
-void app_init2(door **array, int length) {
-	free(*array);
-	*array = malloc(length * sizeof(door));
-	for (int i = 0; i < length; i++) {
-		(*array)[i] = {.controlbits = i}
 	}
 }
 
@@ -73,8 +61,7 @@ void app_init(door active_doors[]){
 		
 	}
 	*/
-	
-}
+
 void init_GPIO_Ports()
 {
 	/*  Function used to set the GPIO configuration to the default reset state ****/
@@ -112,54 +99,91 @@ void systick_Init(void)
 	}
 }
 
+void init_Doors(door *pointer, int length)
+{
+	int SafetyNum = 0;
+	for (int i = 0; i < sizeof(GPIO_Pins); i = i+2)
+	{
+		if (SafetyNum == length) //finns ifall en dörr stängs efter att första gången som programet kollar dörrar
+		{
+			break;
+		}
+		if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pins[i]))
+		{
+		
+			SafetyNum++;
+			pointer->id = i;
+			pointer->controlbits = 0;
+			pointer->time_larm = 1;
+			pointer->time_central_larm = 2;
+			pointer->password = 0;
+			pointer->GPIO_lamp = GPIO_Pins[i + 1];
+			pointer->GPIO_read = GPIO_Pins[i];
+			pointer->larmTick = 0;
+			pointer++;
+			
+		}
+	}
+}
+
 void main(void)
 {
 	init_GPIO_Ports();
-	number_of_activeDoors();			// Hur många dörrar är aktiva?
-	door activeDoors[activeDoors];		// Skapa en array med tillräckligt stor size.
-	app_init2(&activeDoors,active_doors);				// Initiera tillräckligt många structs och lägg till i arrayen.
+
+	int amountOfActiveDoors = 0;
+	detect_Closed_Doors(&amountOfActiveDoors); // Hur många dörrar är aktiva?
+	door active_doors[amountOfActiveDoors];	// Skapa en array med tillräckligt stor size.
+	init_Doors(&active_doors[0], amountOfActiveDoors);
+
+	//app_init2(&activeDoors,active_doors);		// Initiera tillräckligt många structs och lägg till i arrayen.
+
 	systick_Init();
-//	DebugPrintInit();
+	//	DebugPrintInit();
 	//door test1 = {.id = 0, .controlbits = 0, .time_larm = 1, .time_central_larm = 2, .password = 0, .GPIO_lamp = GPIO_Pin_3, .GPIO_read = GPIO_Pin_2, .larmTick = 0};
 	//door test2 = {.id = 1, .controlbits = 0, .time_larm = 1, .time_central_larm = 2, .password = 0, .GPIO_lamp = GPIO_Pin_5, .GPIO_read = GPIO_Pin_4, .larmTick = 0};
 	//door test3 = {.id = 2, .controlbits = 0, .time_larm = 1, .time_central_larm = 2, .password = 0, .GPIO_lamp = GPIO_Pin_1, .GPIO_read = GPIO_Pin_0, .larmTick = 0};
 	//door test4 = {.id = 3, .controlbits = 0, .time_larm = 1, .time_central_larm = 2, .password = 0, .GPIO_lamp = GPIO_Pin_7, .GPIO_read = GPIO_Pin_6, .larmTick = 0};
-//
+	//
 	//door active_doors[4] = {test1,test2,test3,test4};
 
 	while (1)
 	{
 		for (int i = 0; i < sizeof(active_doors); i++)
 		{
-			if(GPIO_ReadInputDataBit(GPIOA, active_doors[i].GPIO_read)){
+			if (GPIO_ReadInputDataBit(GPIOA, active_doors[i].GPIO_read))
+			{
 				active_doors[i].controlbits &= 0xFFFE; //borde va så!
-				//GPIO_ResetBits(GPIOA, active_doors[i].GPIO_lamp);
-			} else{
-				if(!active_doors[i].controlbits & 1){
+													   //GPIO_ResetBits(GPIOA, active_doors[i].GPIO_lamp);
+			}
+			else
+			{
+				if (!active_doors[i].controlbits & 1)
+				{
 					active_doors[i].larmTick = msTicks;
 				}
 				active_doors[i].controlbits |= 1;
 
 				//GPIO_SetBits(GPIOA, active_doors[i].GPIO_lamp);
+			}
+
+			for (int i = 0; i < sizeof(active_doors); i++)
+			{
+				if (active_doors[i].controlbits & 1 && msTicks > (active_doors[i].larmTick + 1000 * 10 * active_doors[i].time_larm))
+				{
+					GPIO_SetBits(GPIOA, active_doors[i].GPIO_lamp);
 				}
-				
-		for (int i = 0; i < sizeof(active_doors); i++)
-		{ 
-			if (active_doors[i].controlbits & 1 && msTicks > (active_doors[i].larmTick + 1000*10* active_doors[i].time_larm))
-			{
-				GPIO_SetBits(GPIOA, active_doors[i].GPIO_lamp);
+
+				else
+				{
+					GPIO_ResetBits(GPIOA, active_doors[i].GPIO_lamp);
+				}
+				if (active_doors[i].controlbits & 1 && msTicks > (active_doors[i].larmTick + 1000 * 10 * active_doors[i].time_central_larm))
+				{
+					//Larma central enheten
+				}
 			}
-			
-			else{
-				GPIO_ResetBits(GPIOA, active_doors[i].GPIO_lamp);
-			}
-			if (active_doors[i].controlbits & 1 && msTicks > (active_doors[i].larmTick + 1000*10* active_doors[i].time_central_larm))
-			{
-				//Larma central enheten
-			}	
 		}
 	}
-}
 }
 
 // Lösenord?
