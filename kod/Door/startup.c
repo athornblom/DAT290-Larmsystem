89e9826ca477afae1751f921595c782529956aa5
Adam Thörnblom <adam.thornblom@gmail.com>
2019-10-09 11:15:07 +0200

    WORKING DOOR THNK

--- kod/Door/startup.c
+++ kod/Door/startup.c
@@ -1,10 +1,3 @@
-/*
- * 	startup.c
- *
- */
-
-//#define DEBUG
-
 #include "stm32f4xx.h"
 #include "stm32f4xx_rcc.h"
 #include "stm32f4xx_rcc.c"
@@ -12,7 +5,6 @@
 #include "stm32f4xx_gpio.h"
 #include "system_stm32f4xx.h"
 #include "startup.h"
-//#include "USARTDebug.h"
 
 void startup(void) __attribute__((naked)) __attribute__((section(".start_section")));
 
@@ -25,77 +17,20 @@ void startup(void)
 		"_exit: B .\n" /* never return */
 	);
 }
-
+// ========================================= DOOOOOORS INIT =================================================
 uint16_t GPIO_Pins[] = {
 	GPIO_Pin_0, GPIO_Pin_1, GPIO_Pin_2, GPIO_Pin_3, GPIO_Pin_4, GPIO_Pin_5,
 	GPIO_Pin_6, GPIO_Pin_7, GPIO_Pin_8, GPIO_Pin_9, GPIO_Pin_10, GPIO_Pin_11,
 	GPIO_Pin_12, GPIO_Pin_13, GPIO_Pin_14, GPIO_Pin_15};
 
-volatile uint32_t msTicks = 0; /* Variable to store millisecond ticks */
-
 void detect_Closed_Doors(int *pointer)
 {
-
 	for (int i = 0; i < sizeof(GPIO_Pins); i = i + 2)
 	{
 		if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pins[i]))
-			;
 		{
 			(*pointer)++;
 		}
-	}
-}
-
-/*
-void app_init(door active_doors[]){
-	int a = 0;
-	int c = 0;
-	for (int i = 0; i < sizeof(GPIO_Pins); i = i+2)
-	{
-		if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pins[i]));
-		{
-			active_doors[a] = {.id = a, .controlbits = 0, .time_larm = 1, .time_central_larm = 2, .password = 4444, .GPIO_lamp = GPIO_Pins[i+1], .GPIO_read = GPIO_Pins[i], .larmTick = 0};
-			a++;
-			c++;
-		}
-		
-	}
-	*/
-
-void init_GPIO_Ports()
-{
-	/*  Function used to set the GPIO configuration to the default reset state ****/
-	GPIO_InitTypeDef init;
-	//GPIO A UTPORTAR
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-	GPIO_StructInit(&init);
-	init.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_3 | GPIO_Pin_5 | GPIO_Pin_7;
-	init.GPIO_Mode = GPIO_Mode_OUT;
-	init.GPIO_OType = GPIO_OType_PP;
-	GPIO_Init(GPIOA, &init);
-
-	//konfigurerar inport GPIO A
-	GPIO_StructInit(&init);
-	init.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_2 | GPIO_Pin_4 | GPIO_Pin_6;
-	init.GPIO_Mode = GPIO_Mode_IN;
-	init.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_Init(GPIOA, &init);
-}
-void SysTick_Handler(void)
-{ /* SysTick interrupt Handler. */
-	msTicks++;
-}
-
-void systick_Init(void)
-{
-	// Initiera SysTick.
-	*((void (**)(void))0x2001C03C) = SysTick_Handler;
-	uint32_t returnCode;
-	returnCode = SysTick_Config(168000000 / 1000); // Genererar ett SysTick-avbrott varje ms.
-
-	if (returnCode != 0)
-	{   // Om inte SysTick_Config lyckas...
-		//typ reboot? bootloops är alltid kul
 	}
 }
 
@@ -126,6 +61,47 @@ void init_Doors(door *pointer, int length)
 	}
 }
 
+//======================================GPIO=========================================================
+void init_GPIO_Ports()
+{
+	/*  Function used to set the GPIO configuration to the default reset state ****/
+	GPIO_InitTypeDef init;
+	//GPIO A UTPORTAR
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	GPIO_StructInit(&init);
+	init.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_3 | GPIO_Pin_5 | GPIO_Pin_7;
+	init.GPIO_Mode = GPIO_Mode_OUT;
+	init.GPIO_OType = GPIO_OType_PP;
+	GPIO_Init(GPIOA, &init);
+
+	//konfigurerar inport GPIO A
+	GPIO_StructInit(&init);
+	init.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_2 | GPIO_Pin_4 | GPIO_Pin_6;
+	init.GPIO_Mode = GPIO_Mode_IN;
+	init.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_Init(GPIOA, &init);
+}
+
+
+// ========================================= SYSTICK ================================================
+volatile uint32_t msTicks = 0; /* Variable to store millisecond ticks */
+void SysTick_Handler(void)
+{ /* SysTick interrupt Handler. */
+	msTicks++;
+}
+void systick_Init(void)
+{
+	// Initiera SysTick.
+	*((void (**)(void))0x2001C03C) = SysTick_Handler;
+	uint32_t returnCode;
+	returnCode = SysTick_Config(168000000 / 1000); // Genererar ett SysTick-avbrott varje ms.
+
+	if (returnCode != 0)
+	{   // Om inte SysTick_Config lyckas...
+		//typ reboot? bootloops är alltid kul
+	}
+}
+
 void main(void)
 {
 	init_GPIO_Ports();
@@ -135,16 +111,16 @@ void main(void)
 	door active_doors[amountOfActiveDoors];	// Skapa en array med tillräckligt stor size.
 	init_Doors(&active_doors[0], amountOfActiveDoors);
 
-	//app_init2(&activeDoors,active_doors);		// Initiera tillräckligt många structs och lägg till i arrayen.
-
 	systick_Init();
-	//	DebugPrintInit();
-	//door test1 = {.id = 0, .controlbits = 0, .time_larm = 1, .time_central_larm = 2, .password = 0, .GPIO_lamp = GPIO_Pin_3, .GPIO_read = GPIO_Pin_2, .larmTick = 0};
-	//door test2 = {.id = 1, .controlbits = 0, .time_larm = 1, .time_central_larm = 2, .password = 0, .GPIO_lamp = GPIO_Pin_5, .GPIO_read = GPIO_Pin_4, .larmTick = 0};
-	//door test3 = {.id = 2, .controlbits = 0, .time_larm = 1, .time_central_larm = 2, .password = 0, .GPIO_lamp = GPIO_Pin_1, .GPIO_read = GPIO_Pin_0, .larmTick = 0};
-	//door test4 = {.id = 3, .controlbits = 0, .time_larm = 1, .time_central_larm = 2, .password = 0, .GPIO_lamp = GPIO_Pin_7, .GPIO_read = GPIO_Pin_6, .larmTick = 0};
-	//
-	//door active_doors[4] = {test1,test2,test3,test4};
+
+	/*
+	door test1 = {.id = 0, .controlbits = 0, .time_larm = 0, .time_central_larm = 2, .password = 0, .GPIO_lamp = GPIO_Pin_3, .GPIO_read = GPIO_Pin_2, .larmTick = 0};
+	door test2 = {.id = 1, .controlbits = 0, .time_larm = 0, .time_central_larm = 2, .password = 0, .GPIO_lamp = GPIO_Pin_5, .GPIO_read = GPIO_Pin_4, .larmTick = 0};
+	door test3 = {.id = 2, .controlbits = 0, .time_larm = 0, .time_central_larm = 2, .password = 0, .GPIO_lamp = GPIO_Pin_1, .GPIO_read = GPIO_Pin_0, .larmTick = 0};
+	door test4 = {.id = 3, .controlbits = 0, .time_larm = 0, .time_central_larm = 2, .password = 0, .GPIO_lamp = GPIO_Pin_7, .GPIO_read = GPIO_Pin_6, .larmTick = 0};
+	
+	door active_doors[4] = {test1,test2,test3,test4};
+	*/
 
 	while (1)
 	{
