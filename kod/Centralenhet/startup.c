86a03323bf81fe2898d2f73e160e4d32568f0f77
Filip Borg <filipborg97@gmail.com>
2019-10-10 11:43:06 +0200

    Lägger till funktioner för kommandohantering och lägesbyten

--- kod/Centralenhet/startup.c
+++ kod/Centralenhet/startup.c
@@ -8,6 +8,8 @@
 #include "stm32f4xx_gpio.h"
 #include "delay.c"
 
+#define MAXCOMMANDLENGTH 10
+
 CanRxMsg RxMessage;
 CanTxMsg TxMessage;
 
@@ -165,6 +167,190 @@ void add_handler(void){
         USARTPrintNum(index);
     }
 }*/
+
+uint8_t msgPrint(CanRxMsg *msg, uint8_t base){
+    USARTPrint("New msg:\n");
+
+    //Skriver ut ID
+    if (msg->IDE == CAN_Id_Standard) {
+        USARTPrint("STD ID: \n");
+        USARTPrintNumBase(msg->StdId, base);
+    } else {
+        USARTPrint("Ext ID: \n");
+        USARTPrintNumBase(msg->ExtId, base);
+    }
+
+    USARTPrint("\nData: \n");
+    for (uint8_t i = 0; i < msg->DLC; i++){
+        USARTPrintNumBase(msg->Data[i],base);
+    }
+    USARTPrint("\n\n");
+}
+
+void confMsg(CanRxMsg *msg){
+    USARTPrint("Nytt konf msg:\n");
+    msgPrint(msg, 16);
+}
+
+uint8_t enterConfMode (void){
+    uint8_t retIndex;
+    CANFilter filter;
+    CANFilter mask;
+    mask.STDID = 0b11110000000;
+    mask.EXDID = 0;
+    mask.IDE = 1;
+    mask.RTR = 1;
+
+    //Avaktiverar alla filter
+    CANdisableAllFilterHandlers();
+
+    //Filter för ID-Begäran
+    filter.STDID = 0b10010000000;
+    filter.EXDID = 0;
+    filter.IDE = 0;
+    filter.RTR = 1;
+
+    //Aktiverar handler för ID begäran
+    if (CANhandlerListNotFull()){
+        //TODO skriv handler för ID och skriv klart raden nedan
+        //retIndex = CANaddFilterHandler(idHandler, &filter, &mask);
+        USARTPrintNum(retIndex);
+        USARTPrint("\n");
+    } else {
+        USARTPrint("Error handler list not empty\n");
+        return 0;
+    }
+
+    //Filter för mottagen konfiguration
+    filter.STDID = 0b01010000000;
+    filter.EXDID = 0;
+    filter.IDE = 0;
+    filter.RTR = 1;
+
+    //Aktiverar handler för ID begäran
+    if (CANhandlerListNotFull()){
+        //TODO skriv handler för mottagen konfiguration och skriv klart raden nedan
+        //retIndex = CANaddFilterHandler(confHandler, &filter, &mask);
+        USARTPrintNum(retIndex);
+        USARTPrint("\n");
+    } else {
+        USARTPrint("Error handler list not empty\n");
+        return 0;
+    }
+    return 1;
+}
+
+uint8_t enterStdMode (void){
+     uint8_t retIndex;
+    CANFilter filter;
+    CANFilter mask;
+    mask.STDID = 0b11110000000;
+    mask.EXDID = 0;
+    mask.IDE = 1;
+    mask.RTR = 1;
+
+    //Avaktiverar alla filter
+    CANdisableAllFilterHandlers();
+
+    //Filter för Larm
+    filter.STDID = 0b00110000000;
+    filter.EXDID = 0;
+    filter.IDE = 0;
+    filter.RTR = 1;
+
+    //Aktiverar handler för larm
+    if (CANhandlerListNotFull()){
+        //TODO skriv handler för larm och skriv klart raden nedan
+        //retIndex = CANaddFilterHandler(larmHandler, &filter, &mask);
+        USARTPrintNum(retIndex);
+        USARTPrint("\n");
+    } else {
+        USARTPrint("Error handler list not empty\n");
+    }
+}
+
+//Jämför två strängar. Returnerar 1 om de är lika. 0 annars.
+uint8_t strEqual(uint8_t *str1, uint8_t *str2){
+    //Loopa så länge vi inte nått slutet på någon av strängarna
+    while (*str1 && *str2) {
+        //Skiljer sig tecknen åt, returnera 0
+        //Flyttar också pekarna
+        if (*str1++ != *str2++){
+            return 0;
+        }
+    }
+
+    //De är endast lika om de hitills varit lika och de slutar samtidigt
+    return *str1 == *str2;
+}
+
+//Kollar om str1 börjar med str2. Returnerar 1 om det är så. 0 annars.
+uint8_t strStartsWith(uint8_t *str1, uint8_t *str2){
+    //Loopa tills vi kommer till slutet av str2
+    while(*str1 && *str2){
+        //Skiljer sig tecknen åt returnera 0
+        //Flyttar också pekaren
+        if (*str1++ != *str2++){
+            return 0;
+        }
+    }
+
+    //str1 slutar innan str2, returnera 0
+    if (*str1 == 0 && *str2 != 0){
+        return 0;
+    }
+
+    return 1;
+}
+
+void Command(uint8_t *command){
+    if (strEqual(command, "test")) {
+        USARTPrint("Hanterar test\n");
+    }
+}
+
+void USARTCommand(void) {
+    //Lista för nuvarande kommando + 1 för att ha plats för terminering
+    static uint8_t currentCommand[MAXCOMMANDLENGTH + 1];
+    static uint8_t index = 0;
+    static uint8_t newCommand = 1;
+
+    if (newCommand) {
+        newCommand = 0;
+        USARTPrint(">>");
+    }
+
+    uint8_t uint8Read;
+
+    if (USARTGet(&uint8Read)){
+        if(uint8Read == '\r' || uint8Read == '\n'){
+            USARTPrint("\n");
+            //Terminering
+            currentCommand[index] = 0;
+            index = 0;
+            newCommand = 1;
+            Command((uint8_t *)(&currentCommand[0]));
+
+        } else {
+            //Check om vi skriver ett för lång kommando
+            if (index >= MAXCOMMANDLENGTH){
+                USARTPrint("\nFor langt kommando\n");
+                index = 0;
+                newCommand = 1;
+            } else {
+                //Skriver ut tecknet
+                uint8_t oneChar[2] = {uint8Read, 0};
+                USARTPrint(oneChar);
+
+                //Lägger till bokstav till kommandot
+                currentCommand[index++] = uint8Read;
+
+                //Terminering
+                currentCommand[index] = 0;
+            }
+        }
+    }
+}
 
 void main(void) {
     USARTConfig();
