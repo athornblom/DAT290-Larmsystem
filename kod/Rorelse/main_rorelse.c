1c3de1c7eac921c973a25b3fcc97b75f9f8da69e
Erik Nilsson <erik1.nilsson2@gmail.com>
2019-10-11 11:06:51 +0200

    Tagit bort redundant kod, ändrat lite i kommentarer och indenterat koden korrekt

--- kod/Rorelse/main_rorelse.c
+++ kod/Rorelse/main_rorelse.c
@@ -29,7 +29,7 @@ __asm volatile(
 
 
 
-// Struct för avståndsmätare
+// Struct för rörelsesensorer
 typedef struct MotionSensors {
 	char id, controlbits;							// Id och 8 kontrollbitar tex den minst signifikanta biten är ifall sensorn är aktiv eller ej.
 	short password;									// 4 sifferig kod för att aktivera/avaktivera sensorn.
@@ -157,63 +157,33 @@ void main(void){
 	MotionSensor motionSensors[2] = {motion1, motion2};
 	
 	while(1){
-		//DebugPrint("Start\n");
-		// Delayverision
-		/*
-		GPIO_ResetBits(GPIOA, motion1.trig); // Vill ha en fin hög
-		delay_micro(2);
-		GPIO_SetBits(GPIOA,motion1.trig); // Aktivera triggpuls
-		delay_micro(10);
-		GPIO_ResetBits(GPIOA, motion1.trig); // Avaktivera triggpuls 
-		
-		while(!GPIO_ReadInputDataBit(GPIOA, motion1.echo)){ // Vänta på svar från echo
-		}
-	
-		motion1.pulseEcho = microTicks;
-		while(GPIO_ReadInputDataBit(GPIOA, motion1.echo)){ // Mät hur länge echopulsen är hög
-			//highcount++;
-		}
-		
-	
-		
-		motion1.cm = microTicks - motion1.pulseEcho/58;
-		if(motion1.cm < motion1.alarm){ // Upptäcker sensorn något som är för nära?
-			GPIO_SetBits(GPIOA, GPIO_Pin_4);	// Tänd lampa
-			DebugPrint("1\n");
-		}
-		else{
-			GPIO_ResetBits(GPIOA, GPIO_Pin_4);	// Släck lampa
-			DebugPrint("0\n");
-		}		
-		delay_micro(200000);
-*/
 		
 		// Pollingverision avståndssensor
-		for(int i = 0; i*4 < sizeof(motionSensors); i++){ // Itererar koden för alla avståndsmätare
-		if(motionSensors[i].controlbits & 1){ // Är sensorn aktiverad?
-			if(microTicks >= motionSensors[i].pulseTrig){ // Är trigpulsen klar?
-				GPIO_ResetBits(GPIOA, motionSensors[i].pinTrig);	// Avaktivera triggerpuls
+		for(int i = 0; i*4 < sizeof(motionSensors); i++){ // Itererar koden för alla rörelsesensorer
+			if(motionSensors[i].controlbits & 1){ // Är sensorn aktiverad?
+				if(microTicks >= motionSensors[i].pulseTrig){ // Är trigpulsen klar?
+					GPIO_ResetBits(GPIOA, motionSensors[i].pinTrig);	// Avaktivera triggerpuls
+				}
+				if(microTicks >= motionSensors[i].pulseDelay){  // Är triggfördröjningen klar?
+					GPIO_SetBits(GPIOA, motionSensors[i].pinTrig);	// Aktivera triggerpuls
+					motionSensors[i].pulseTrig = microTicks + 10; // Triggpuls 10µs
+					motionSensors[i].pulseDelay = microTicks + 60000;	// Fördröjning mellan triggerpulserna, 60ms
+				}
+				if(!(motionSensors[i].controlbits & (1 << 1)) && GPIO_ReadInputDataBit(GPIOA, motionSensors[i].pinEcho)){ // Är echo hög för första gången?
+					motionSensors[i].pulseEcho = microTicks; // Början av echopulsen.
+					motionSensors[i].controlbits |= 1 << 1;  // Ettställer kontrollbit 1.
+				}
+				if ((motionSensors[i].controlbits & (1 << 1)) && !GPIO_ReadInputDataBit(GPIOA, motionSensors[i].pinEcho)) {	// Är echo låg för första gången?
+					motionSensors[i].cm = (microTicks - motionSensors[i].pulseEcho)/58; // Sekunder tills echo kommer tillbaks.
+					motionSensors[i].controlbits &= 0xFD;	// Nollställer kontrollbit 1.
+				}
+				if(motionSensors[i].cm < motionSensors[i].alarm){	// Upptäcker sensorn något som är för nära?
+					GPIO_SetBits(GPIOA, motionSensors[i].pinLamp);	// Tänd lampa.
+				}
+				else{
+					GPIO_ResetBits(GPIOA, motionSensors[i].pinLamp);	// Släck lampa.
+				}
 			}
-			if(microTicks >= motionSensors[i].pulseDelay){  // Är triggfördröjningen klar?
-				GPIO_SetBits(GPIOA, motionSensors[i].pinTrig);	// Aktivera triggerpuls
-				motionSensors[i].pulseTrig = microTicks + 10; // Triggpuls 10µs
-				motionSensors[i].pulseDelay = microTicks + 60000;	// Fördröjning mellan triggerpulserna, 60ms
-			}
-			if(!(motionSensors[i].controlbits & (1 << 1)) && GPIO_ReadInputDataBit(GPIOA, motionSensors[i].pinEcho)){ // Är echo hög för första gången?
-				motionSensors[i].pulseEcho = microTicks; // Början av echopulsen.
-				motionSensors[i].controlbits |= 1 << 1;  // Ettställer kontrollbit 1.
-			}
-			if ((motionSensors[i].controlbits & (1 << 1)) && !GPIO_ReadInputDataBit(GPIOA, motionSensors[i].pinEcho)) {	// Är echo låg för första gången?
-				motionSensors[i].cm = (microTicks - motionSensors[i].pulseEcho)/58; // Sekunder tills echo kommer tillbaks.
-				motionSensors[i].controlbits &= 0xFD;	// Nollställer kontrollbit 1.
-			}
-			if(motionSensors[i].cm < motionSensors[i].alarm){	// Upptäcker sensorn något som är för nära?
-				GPIO_SetBits(GPIOA, motionSensors[i].pinLamp);	// Tänd lampa.
-			}
-			else{
-				GPIO_ResetBits(GPIOA, motionSensors[i].pinLamp);	// Släck lampa.
-			}
-		}
 		}
 		
 		if(!GPIO_ReadInputDataBit(GPIOA, vibration1.pinDO)){
@@ -222,34 +192,6 @@ void main(void){
 		else{
 			GPIO_ResetBits(GPIOA, vibration1.pinLamp);
 		}
-	
-		
-		
-		//if (GPIO_ReadInputDataBit(GPIOB, motion2.echo) == Bit_RESET) {  // Vibration triggad
-		/* Återanvänder variabler i MotionSensor till annat, Todo: i framtiden bör det nog finnas en egen struct för vibration.
-		 * pulseEcho  = antal vibrationer
-		 * pulseDelay = max antal vibrationer på 1 sekund innan det larmar
-		 * pulseTrig  = tidsdelta 
-		 * 
-		 * Vid mätning utan delay mellan loop-cyklarna verkade 10k vara ett rimligt värde för pulseDelay.
-		 * Detta bör mätas om när loopen är färdigskriven då timingen kommer att ha ändrats
-		
-			if (!motion2.pulseEcho) {
-				motion2.pulseTrig = microTicks; // Tidstamp för första vibrationen			
-			}
-			motion2.pulseEcho++;
-		}
-			
-						
-		if (microTicks > (motion2.pulseTrig + 1000000))  {  // 1 Sekund efter första vibration
-			if (motion2.pulseEcho > motion2.pulseDelay) {
-				// Larma
-			}
-			motion2.pulseTrig = 0;
-			motion2.pulseEcho= 0;
-		}
-		*/
-		// microTicks bör ställas till 0 vid lämpligt tillfälle, max värde = 72 min
 		
 	}
 
