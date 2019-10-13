59388649d86659c318a0973510e206c692fec15d
Filip Borg <filipborg97@gmail.com>
2019-10-13 12:00:28 +0200

    Flyttar strängfunktioner till egena filer

--- kod/Centralenhet/startup.c
+++ kod/Centralenhet/startup.c
@@ -7,6 +7,7 @@
 #include "stm32f4xx_rcc.h"
 #include "stm32f4xx_gpio.h"
 #include "delay.c"
+#include "stringFunc.h"
 
 #define MAXCOMMANDLENGTH 10
 
@@ -263,40 +264,6 @@ uint8_t enterStdMode (void){
         USARTPrintNum(retIndex);
         USARTPrint("\n");
     } else {
-        return 0;
-    }
-
-    return 1;
-}
-
-//Jämför två strängar. Returnerar 1 om de är lika. 0 annars.
-uint8_t strEqual(uint8_t *str1, uint8_t *str2){
-    //Loopa så länge vi inte nått slutet på någon av strängarna
-    while (*str1 && *str2) {
-        //Skiljer sig tecknen åt, returnera 0
-        //Flyttar också pekarna
-        if (*str1++ != *str2++){
-            return 0;
-        }
-    }
-
-    //De är endast lika om de hitills varit lika och de slutar samtidigt
-    return *str1 == *str2;
-}
-
-//Kollar om str1 börjar med str2. Returnerar 1 om det är så. 0 annars.
-uint8_t strStartsWith(uint8_t *str1, uint8_t *str2){
-    //Loopa tills vi kommer till slutet av str2
-    while(*str1 && *str2){
-        //Skiljer sig tecknen åt returnera 0
-        //Flyttar också pekaren
-        if (*str1++ != *str2++){
-            return 0;
-        }
-    }
-
-    //str1 slutar innan str2, returnera 0
-    if (*str1 == 0 && *str2 != 0){
         return 0;
     }
 
