e4703c27cfc07638fedd0ebc486d63af44226d06
Filip Borg <filipborg97@gmail.com>
2019-10-14 18:23:41 +0200

    Omordnar funktioner

--- kod/lib/keypad.c
+++ kod/lib/keypad.c
@@ -21,97 +21,10 @@ static const uint8_t keyMatrix[KEYPAD_ROWS][KEYPAD_COLS] = {{0x1, 0x2, 0x3, 0xa}
 static const uint16_t colPins[KEYPAD_COLS] = {GPIO_Pin_8, GPIO_Pin_9, GPIO_Pin_10, GPIO_Pin_11};
 static const uint16_t rowPins[KEYPAD_ROWS] = {GPIO_Pin_12, GPIO_Pin_13, GPIO_Pin_14, GPIO_Pin_15};
 
-//Initierar keypad för port PE8-15 med avbrott från gul banankontakt kopplad till PE0
-void keypadnit(){
-    //Tnitsierar buffer
-    keyBuffer = &realKeyBuffer;
-    bufferInit(keyBuffer);
-    
-    //Startar klockorna för PE och för SYSCFG (för att kunna aktivera EXTI avbrott)
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
-    
-    //Konfigurerar pinnarna i PE för keypad
-    GPIO_InitTypeDef GPIO_InitStructure;
-    GPIO_StructInit(&GPIO_InitStructure);
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
-    //Konfigurerar PE0 och PE8-11 som ingångar pull-down
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10 | GPIO_Pin_11;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
-    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
-    GPIO_Init(GPIOE, &GPIO_InitStructure);
-
-    //Konfigurerear PE12-15 som utgångar push-pull
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-    GPIO_Init(GPIOE, &GPIO_InitStructure);
-    
-    //Konfigurerar avbrott för PE0 3 steg
-    //1 aktiverar EXTI
-    setEXTI0(ENABLE);
-    
-    //2 konfigurera NVIC
-    NVIC_InitTypeDef NVIC_InitStructure;
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); 
-    NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_Init(&NVIC_InitStructure);
-    
-    //3 Sätter avbrottsvektorn manuellt
-    *((void (**)(void) ) (0x2001C000 + 0x58) ) = keyPad_IRQ;
-    
-    //Skriver raderna höga för att detektera knapptryckning som genererar avbrott
-    setRows(1);
-}
-
 //Läser senaste tecknet från keypaden till dest
 //Returnerar 1 om det fanns att läsa 0 annars
 uint8_t readKeypad(uint8_t *dest){
     return bufferGet(keyBuffer, dest);
-}
-
-//Avbrottsrutin för keypad
-void keyPad_IRQ(void){
-    //TODO REMOVE
-    //USARTPrint("IRQ*");
-    //TODO skulle det fungera bättre med någon delay någon stans
-    //för man misslyckas med läsningar rätt offta
-    
-    //Avaktiverar EXTI0 för att undvika rekursion
-    setEXTI0(DISABLE);
-        
-    //Sätter raderna låga för att sedan sätta dem en och en
-    setRows(0);
-        
-    //Loopar igenom alla rader
-    for (uint8_t row = 0; row < KEYPAD_ROWS; row++){
-        //Sätter raden hög
-        GPIO_SetBits(GPIOE, rowPins[row]);
-        //Läser av kolumnerna och sparar knapptryckningar i bufferter
-        uint16_t activeCols = GPIO_ReadInputData(GPIOE);
-        for (int col = 0; col < KEYPAD_COLS; col++){
-            if (activeCols & colPins[col]){
-                bufferPut(keyBuffer, keyMatrix[row][col]);
-                //TODO REMOVE
-                //USARTPrint("FOUND*");
-            }
-        }
-        
-        //Sätter raden låg
-        GPIO_ResetBits(GPIOE, rowPins[row]);
-    }
-    
-    //Sätter raderna höga efter att vi har utfört avbrottet
-    setRows(1);
-
-    //Clearar avbrottet
-    EXTI_ClearITPendingBit(EXTI_Line0);
-    
-    //Aktiverar avbrott igen
-    setEXTI0(ENABLE);
 }
 
 void setRows(uint8_t set){
@@ -130,13 +43,100 @@ void setRows(uint8_t set){
 void setEXTI0(FunctionalState state){
     //konfigurera SYSCFG
     SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE, EXTI_PinSource0);
-    
+
     //Konfigurerar EXTI0
     //state avgör om den är aktiv eller inte
     EXTI_InitTypeDef EXTI_InitStructure;
     EXTI_InitStructure.EXTI_Line = EXTI_Line0;
-    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt; 
+    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
     EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
     EXTI_InitStructure.EXTI_LineCmd = state;
     EXTI_Init(&EXTI_InitStructure);
+}
+
+//Avbrottsrutin för keypad
+void keyPad_IRQ(void){
+    //TODO REMOVE
+    //USARTPrint("IRQ*");
+    //TODO skulle det fungera bättre med någon delay någon stans
+    //för man misslyckas med läsningar rätt offta
+
+    //Avaktiverar EXTI0 för att undvika rekursion
+    setEXTI0(DISABLE);
+
+    //Sätter raderna låga för att sedan sätta dem en och en
+    setRows(0);
+
+    //Loopar igenom alla rader
+    for (uint8_t row = 0; row < KEYPAD_ROWS; row++){
+        //Sätter raden hög
+        GPIO_SetBits(GPIOE, rowPins[row]);
+        //Läser av kolumnerna och sparar knapptryckningar i bufferter
+        uint16_t activeCols = GPIO_ReadInputData(GPIOE);
+        for (int col = 0; col < KEYPAD_COLS; col++){
+            if (activeCols & colPins[col]){
+                bufferPut(keyBuffer, keyMatrix[row][col]);
+                //TODO REMOVE
+                //USARTPrint("FOUND*");
+            }
+        }
+
+        //Sätter raden låg
+        GPIO_ResetBits(GPIOE, rowPins[row]);
+    }
+
+    //Sätter raderna höga efter att vi har utfört avbrottet
+    setRows(1);
+
+    //Clearar avbrottet
+    EXTI_ClearITPendingBit(EXTI_Line0);
+
+    //Aktiverar avbrott igen
+    setEXTI0(ENABLE);
+}
+
+//Initierar keypad för port PE8-15 med avbrott från gul banankontakt kopplad till PE0
+void keypadnit(){
+    //Tnitsierar buffer
+    keyBuffer = &realKeyBuffer;
+    bufferInit(keyBuffer);
+
+    //Startar klockorna för PE och för SYSCFG (för att kunna aktivera EXTI avbrott)
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+
+    //Konfigurerar pinnarna i PE för keypad
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_StructInit(&GPIO_InitStructure);
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+    //Konfigurerar PE0 och PE8-11 som ingångar pull-down
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10 | GPIO_Pin_11;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
+    GPIO_Init(GPIOE, &GPIO_InitStructure);
+
+    //Konfigurerear PE12-15 som utgångar push-pull
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_Init(GPIOE, &GPIO_InitStructure);
+
+    //Konfigurerar avbrott för PE0 3 steg
+    //1 aktiverar EXTI
+    setEXTI0(ENABLE);
+
+    //2 konfigurera NVIC
+    NVIC_InitTypeDef NVIC_InitStructure;
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+    NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    //3 Sätter avbrottsvektorn manuellt
+    *((void (**)(void) ) (0x2001C000 + 0x58) ) = keyPad_IRQ;
+
+    //Skriver raderna höga för att detektera knapptryckning som genererar avbrott
+    setRows(1);
 }
